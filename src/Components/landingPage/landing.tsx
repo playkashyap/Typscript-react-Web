89c2182390a190529611b5339e5dd37a3cdb385a
Shubham Kumar Prajapati <jackwill021@gmail.com>
2023-07-17 09:24:53 +0530

    changes

--- src/Components/landingPage/landing.tsx
+++ src/Components/landingPage/landing.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Box, CardActionArea } from '@mui/material';
+import backgroung from '../../assets/cover.jpg'
+import Paper from '@mui/material/Paper';
 
 export default function Landing() {
     return (
-        <div>
-            <h1>hello</h1>
-        </div>
-    )
+        <Box>
+            <Paper  elevation = {3} style={{ maxHeight : 350 , overflow : 'hidden', borderRadius : 0}}>
+                <img src={backgroung} alt="" style={{ width: '100vw' , marginTop : '-10%'}} />
+            </Paper>
+            <Card sx={{ maxWidth: '100vw' }}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Box>
+    );
 }
