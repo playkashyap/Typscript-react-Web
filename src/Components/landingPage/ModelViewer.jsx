76a50b34699cac0357ffcc8686a5051aa767765e
Shubham Kumar Prajapati <jackwill021@gmail.com>
2023-10-30 16:29:38 +0530

    updates

--- src/Components/landingPage/ModelViewer.jsx
+++ src/Components/landingPage/ModelViewer.jsx
@@ -3,12 +3,12 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import GltfModel from './GltfModel';
 
-const ModelViewer = ({ modelPath, scale = 1000, position = [0, 0, 0] }) => {
+const ModelViewer = ({ modelPath, scale = 40, position = [0, 0, 0] }) => {
     return (
         <Canvas
-            camera={{ position: [2, 0, 12.25], fov: 15 }}
+            // camera={{ position: [2, 0, 12.25], fov: 15 }}
             style={{
-                backgroundColor: '#111a21',
+                // backgroundColor: '#111a21',
                 width: '100vw',
                 height: '100vh',
             }}
