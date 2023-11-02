44df504203a17dfb034b020b5cac01dc19c87746
Shubham Kumar Prajapati <jackwill021@gmail.com>
2023-11-02 13:03:42 +0530

    updates

--- src/Components/landingPage/ModelViewer.tsx
+++ src/Components/landingPage/ModelViewer.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import GltfModel from './GltfModel';
+import './background.scss';
 
 const ModelViewer = (props: any) => {
     const [modelPath, setModelPath] = React.useState();
@@ -17,15 +18,13 @@ const ModelViewer = (props: any) => {
 
     return (
         <Canvas
-            // camera={{ position: [2, 0, 12.25], fov: 15 }}
+            className="background"
             style={{
-                backgroundColor: '#111a21',
                 width: '100vw',
                 height: '500px',
             }}
         >
             <ambientLight intensity={1} />
-
             <pointLight position={[0, 0, 0]} />
             <hemisphereLight intensity={1} />
             <Suspense fallback={null}>
