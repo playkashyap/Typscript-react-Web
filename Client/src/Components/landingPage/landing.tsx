8e72e0277b565a9b87a9f1be212db0c6ef86e7f7
Shubham kumar prajapati <66512062+playkashyap@users.noreply.github.com>
2023-12-29 16:23:07 +0530

    updates

--- Client/src/Components/landingPage/landing.tsx
+++ Client/src/Components/landingPage/landing.tsx
@@ -2,6 +2,7 @@ import ModelViewer from './ModelViewer';
 
 export default function Landing() {
     const path : any = "/model.glb";
+    // const path : any = "/gladiator.glb";    
     const scale : number = 3;
     return (
         <>
